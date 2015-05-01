a628b10be0e2c31074b036f05e1dab92f9180dcc
sakuda-ryo <sakuda-ryo@dmm.com>
2015-05-01 19:01:21 +0900

    カスタムビュー追加　（途中）

---
+++ Haibi/ResultView.h
@@ -0,0 +1,13 @@
+//
+//  ResultView.h
+//  Haibi
+//
+//  Created by sakuda-ryo on 2015/05/01.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultView : UIView
+
+@end
---
+++ Haibi/ResultView.m
@@ -0,0 +1,21 @@
+//
+//  ResultView.m
+//  Haibi
+//
+//  Created by sakuda-ryo on 2015/05/01.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import "ResultView.h"
+
+@implementation ResultView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
